9cb6c08c6d422454c1f79057b26fe935743aef1f
Nguyen Quang Thao <loozzi.tv@gmail.com>
2021-12-06 17:36:54 +0700

    ..

--- Giai de thi/de so 4/sdb/main.cpp
+++ Giai de thi/de so 4/sdb/main.cpp
@@ -7,46 +7,25 @@ void init(){
     freopen("sdb.out", "w", stdout);
 }
 
-struct dt{
-    long long vl, id;
-    bool c;
-};
-
-bool cmp1(dt a, dt b){
-    return a.vl < b.vl;
-}
-
-bool cmp2(dt a, dt b){
-    return a.id < b.id;
-}
-
 void process(){
-    int n, tmp;
+    int n;
     cin >> n;
-    vector<dt> a;
-    for(int i = 0 ; i < n; i++){
-        cin >> tmp;
-        dt tmp1;
-        tmp1.id = i;
-        tmp1.vl = tmp;
-        tmp1.c = true;
-        a.push_back(tmp1);
+    long long a[n];
+    map<long long, int> b;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        b[a[i]]++;
     }
-    sort(a.begin(), a.end(), cmp1);
-    for(int i = 0; i < a.size() - 1; i++){
-        if(a[i].vl == a[i+1].vl){
-            a[i].c = a[i+1].c = false;
-        }
+    map<long long, int>::iterator ii;
+    int res = 0;
+    for(ii = b.begin(); ii != b.end(); ii++){
+        if(ii->second == 1)
+            res++;
     }
-    sort(a.begin(), a.end(), cmp2);
-    vector<int> res;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i].c)
-            res.push_back(a[i].vl);
-    }
-    cout << res.size() << endl;
-    for(int i = 0; i < res.size(); i++){
-        cout << res[i] << endl;
+    cout << res << endl;
+    for(int i = 0; i < n; i++){
+        if(b[a[i]] == 1)
+            cout << a[i] << endl;
     }
 
 }
